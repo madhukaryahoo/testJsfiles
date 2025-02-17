5c2cba371ad9a2182a53910885d02f4b4e0dc6ef
madhukaryahoo <madhukar.kumbhar@yahoo.com>
2025-02-17 11:01:39 +0000

    Add code

---
+++ .devcontainer/01_variabls.js
@@ -0,0 +1,12 @@
+const accountId = 123;
+let accountEmail = 'test@test.com';
+let accountPass  = '12334';
+accountCity = "Pune"; 
+
+//accountId = 2;
+accountEmail = 'www@test.com';
+accountPass  = '333';
+ 
+console.table([accountId, accountEmail, accountPass, accountCity, accountState]); 
+
+
